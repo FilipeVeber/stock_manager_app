5b9d4c427e5623c4134475946afcc2ade2a778ad
Filipe Veber <filipeveber73@gmail.com>
2024-03-24 22:03:58 -0300

    feat: Listing shares and buy/sell directly in list view

--- lib/pages/wallet_page.dart
+++ lib/pages/wallet_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:stock_manager_app/controllers/stock_controller.dart';
 import 'package:stock_manager_app/controllers/wallet_controller.dart';
 import 'package:stock_manager_app/models/wallet.dart';
 
-class WalletPage extends StatelessWidget {
+class WalletPage extends StatefulWidget {
+  @override
+  State<WalletPage> createState() => _WalletPageState();
+}
+
+class _WalletPageState extends State<WalletPage> {
   WalletController walletController = WalletController();
+
+  StockController stockController = StockController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +37,8 @@ class WalletPage extends StatelessWidget {
 
   Widget buildDataLayout(Wallet wallet) {
     final TextEditingController textEditingController = TextEditingController();
+    final TextEditingController textEditingControllerQuantity =
+        TextEditingController();
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -38,7 +48,7 @@ class WalletPage extends StatelessWidget {
           child: Row(
             children: [
               Text(
-                "Balance ${wallet.balance}",
+                "Balance: ${wallet.balance}",
                 style: const TextStyle(fontSize: 20),
               ),
               const SizedBox(
@@ -85,23 +95,107 @@ class WalletPage extends StatelessWidget {
         ),
         Flexible(
           child: ListView.builder(
-            scrollDirection: Axis.vertical,
             itemCount: wallet.stocks.length,
             itemBuilder: (_, int index) {
               var stock = wallet.stocks[index];
               return Card(
-                child: Row(
-                  children: [
-                    Text(stock.symbol),
-                    const SizedBox(width: 8),
-                    Text("${stock.quantity}"),
-                    const SizedBox(width: 8),
-                    Text("${stock.averagePrice}"),
-                  ],
+                child: ListTile(
+                  title: Row(
+                    children: [
+                      Text(stock.symbol),
+                      const Text(" - "),
+                      Text("Quantity: ${stock.quantity}"),
+                      const Text(" - "),
+                      Expanded(child: Text("Avg price: ${stock.averagePrice}")),
+                      ElevatedButton(
+                        child: const Text("Sell"),
+                        onPressed: () {
+                          stockController.sellStock(
+                              stock.symbol,
+                              int.parse(
+                                  textEditingControllerQuantity.value.text));
+                        },
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: TextFormField(
+                          controller: textEditingControllerQuantity,
+                          decoration: const InputDecoration(
+                            labelText: "Quantity",
+                          ),
+                          keyboardType: const TextInputType.numberWithOptions(),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                                RegExp(r'^-?\d+')),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      ElevatedButton(
+                        child: const Text("Buy"),
+                        onPressed: () {
+                          stockController.buyStock(
+                              stock.symbol,
+                              int.parse(
+                                  textEditingControllerQuantity.value.text));
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               );
             },
           ),
+          // child: GridView.builder(
+          //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          //       crossAxisCount: 10),
+          //   itemCount: wallet.stocks.length,
+          //   itemBuilder: (_, int index) {
+          //     var stock = wallet.stocks[index];
+          //     return Card(
+          //       shape: Border.all(
+          //         color: Colors.deepPurple,
+          //         width: 2,
+          //       ),
+          //       child: Padding(
+          //         padding: const EdgeInsets.all(8.0),
+          //         child: Column(
+          //           children: [
+          //             Text(stock.symbol),
+          //             Text("Quantity: ${stock.quantity}"),
+          //             TextFormField(
+          //               style: const TextStyle(fontSize: 5),
+          //               controller: textEditingControllerQuantity,
+          //               decoration: const InputDecoration(
+          //                 labelText: "Quantity",
+          //               ),
+          //               keyboardType: const TextInputType.numberWithOptions(),
+          //               inputFormatters: [
+          //                 FilteringTextInputFormatter.allow(RegExp(r'^-?\d+')),
+          //               ],
+          //             ),
+          //             Row(
+          //               children: [
+          //                 SizedBox(
+          //                     width: 10,
+          //                     child: ElevatedButton(
+          //                         onPressed: () {},
+          //                         child: const Icon(Icons.add))),
+          //                 ElevatedButton(
+          //                     onPressed: () {}, child: const Text("Sell")),
+          //               ],
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+          //     );
+          //   },
+          // ),
         )
       ],
     );
